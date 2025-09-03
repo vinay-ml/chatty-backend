e345f38600ce7925a453c7a5de1deceb95724727
vinay-ml <vinay.mern@gmail.com>
2025-09-03 15:17:47 +0530

    changes are done

--- src/features/auth/controllers/signin.ts
+++ src/features/auth/controllers/signin.ts
@@ -9,6 +9,7 @@ import { loginSchema } from '@auth/schemes/signin';
 import { IAuthDocument } from '@auth/interfaces/auth.interface';
 import { userService } from '@service/db/user.service';
 import { IUserDocument } from '@user/interfaces/user.interface';
+import { mailTransport } from '@service/emails/mail.transport';
 
 export class SignIn {
   @joiValidation(loginSchema)
